b4c88e79189a2b76debb579ba56b5373c5e579ea
WojciechSwiderski <wojtek.swiderski8@gmail.com>
2022-11-29 23:36:11 +0100

    added content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'frontend-mentor';
+  pageViews: number = 100;
+  yearly: boolean = false;
+
+  get price(): number {
+    const fullPrice = (this.pageViews * 16) / 100;
+    return this.yearly ? 0.75 * fullPrice : fullPrice;
+  }
 }
---
+++ src/app/components/price/price.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-price',
+  templateUrl: './price.component.html',
+  styleUrls: ['./price.component.scss'],
+})
+export class PriceComponent {
+  @Input() price: number = 0;
+}
