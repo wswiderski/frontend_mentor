9c8d20962cf314481b087da6a0d3099da385c978
WojciechSwiderski <wojtek.swiderski8@gmail.com>
2022-12-05 00:09:01 +0100

    finished range input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -9,8 +9,28 @@ export class AppComponent {
   pageViews: number = 100;
   yearly: boolean = false;
 
+  min = 50;
+  max = 150;
+
+  constructor(private readonly renderer: Renderer2) {}
+
   get price(): number {
     const fullPrice = (this.pageViews * 16) / 100;
     return this.yearly ? 0.75 * fullPrice : fullPrice;
+  }
+
+  get priceInputBackground(): string {
+    const percentage = this.getPercentage();
+    return `linear-gradient(to right, hsl(174, 77%, 80%) 0%, hsl(174, 77%, 80%) ${percentage}%, hsl(223, 50%, 87%) ${percentage}%, hsl(223, 50%, 87%) 100%)`;
+  }
+
+  private getPercentage(): number {
+    if (this.pageViews === this.min) {
+      return 0;
+    }
+    if (this.pageViews === this.max) {
+      return 100;
+    }
+    return (100 * (this.pageViews - this.min)) / (this.max - this.min);
   }
 }
