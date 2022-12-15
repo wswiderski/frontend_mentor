f12e5d97203359595ea1b88a2fba87baa33ea772
WojciechSwiderski <wojtek.swiderski8@gmail.com>
2022-12-15 20:44:13 +0100

    added masking to the card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,9 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AppComponent } from './app.component';
 import { TimerComponent } from './timer/timer.component';
 import { FlipperItemComponent } from './flipper-item/flipper-item.component';
-import { MaskCircleComponent } from './flipper-item/mask-circle/mask-circle.component';
 
 @NgModule({
-  declarations: [AppComponent, TimerComponent, FlipperItemComponent, MaskCircleComponent],
+  declarations: [AppComponent, TimerComponent, FlipperItemComponent],
   imports: [BrowserModule, FontAwesomeModule],
   providers: [],
   bootstrap: [AppComponent],
