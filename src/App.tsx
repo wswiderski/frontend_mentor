0cb0891c9516beab8b3569bdf3416531820569cb
WojciechSwiderski <wojtek.swiderski8@gmail.com>
2023-04-13 20:50:10 +0200

    work_in_progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,32 @@
-import "./App.css";
+import styles from "./App.module.scss";
 
-function App() {
-  return <div></div>;
-}
+import logo from "./assets/logo.svg";
+import illustration from "./assets/illustration-mockups.svg";
+
+const App = () => {
+  return (
+    <div className={styles.container}>
+      <nav>
+        <img src={logo} alt="Logo" />
+      </nav>
+      <section>
+        <img src={illustration} alt="Illustration" />
+        <div>
+          <h1>
+            Build The Community <br />
+            Your Fans Will Love
+          </h1>
+          <p>
+            Huddle re-imagines the way we build communities. You have a voice,
+            but so does your audience. Create connections with your users as you
+            engage in genuine discussion. Register
+          </p>
+          <button>Register</button>
+        </div>
+      </section>
+      <footer></footer>
+    </div>
+  );
+};
 
 export default App;
