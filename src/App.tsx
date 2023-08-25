43b3d9cf5c431db0c29483ef588d3d9703a0bd60
WojciechSwiderski <wojtek.swiderski8@gmail.com>
2023-08-25 11:02:51 +0200

    setting up new project

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
-import styles from "./App.module.scss";
-
-import { motion } from "framer-motion";
-
-const App = () => {
-  return <div>Test</div>;
-};
+function App() {
+  return <></>;
+}
 
 export default App;
